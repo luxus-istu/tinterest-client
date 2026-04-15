96b5483f9f1f6b6cb36651720fd07a949365b22e
SS1GGzxc <sterletivakiril@gmail.com>
2026-04-15 13:27:00 +0400

    feat: migrate app layout and globals to src directory and add HeroUI agent documentation

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import Theme from '../providers/Theme'
+import './globals.css'
+import { Manrope } from "next/font/google"
+
+const manrope = Manrope({
+  subsets: ['cyrillic', 'latin'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en' className={manrope.className} suppressHydrationWarning>
+      <body className="bg-background text-foreground">
+        <Theme>{children}</Theme>
+      </body>
+    </html>
+  )
+}
