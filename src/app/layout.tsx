b22818c5ceaca84c90d6b5954e84ca2770018541
SS1GGzxc <sterletivakiril@gmail.com>
2026-04-27 13:37:02 +0400

    feat: add Vercel Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Theme from '../providers/Theme'
 import { Manrope } from "next/font/google"
 import "../styles/globals.css";
 import type { ReactNode } from 'react'
+import { Analytics } from '@vercel/analytics/next';
 
 const manrope = Manrope({
   subsets: ['cyrillic', 'latin'],
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en" className={manrope.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <Theme>{children}</Theme>
+        <Analytics />
       </body>
     </html>
   )
