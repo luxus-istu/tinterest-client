dfab67fd29fcbce65a7ea081daedc42dc7742ac5
Denfoxjhg2 <118984331+Denfoxjhg2@users.noreply.github.com>
2026-05-17 22:36:40 +0400

    fix: fixing global types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,9 +4,3 @@ import { ErrorResponseSchema, UserSchema } from "./schemes";
 export type User = z.infer<typeof UserSchema>;
 export type ProfileResponse = User;
 export type ErrorResponse = z.infer<typeof ErrorResponseSchema>;
-<<<<<<< HEAD
-export type WorkInfo = z.infer<typeof WorkInfoSchema>;
-export type CommunicationPreference = z.infer<typeof CommunicationPreferenceSchema>;
-export type Interest = z.infer<typeof InterestSchema>;
-=======
->>>>>>> origin/main
